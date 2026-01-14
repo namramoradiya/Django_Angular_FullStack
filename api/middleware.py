1557e75c2b13f6e840e813cc96fb6b90f7bf39ce
namramoradiya <moradiyanamra@gmail.com>
2026-01-14 15:25:13 +0530

    Custom Middleware is added

--- api/middleware.py
+++ api/middleware.py
@@ -5,14 +5,12 @@ class RequestLogMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Before view execution
         print(
-            f"[MIDDLEWARE] {datetime.now()} | "
+            f"[MIDDLEWARE] {datetime.now()} | "                                                                                                                                                                                                                                                                                                                 
             f"{request.method} | {request.path}"
         )
 
         response = self.get_response(request)
 
-        # After view execution
         response['X-CLIENT-TYPE'] = 'Angular-App'
         return response
